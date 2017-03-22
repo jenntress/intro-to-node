40707af816c834fb684c4ea9a50f0fb623a1191b
jenntress <jenntress@gmail.com>
2017-03-21 21:32:29 -0600

    Configured basic web server using http node module

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
 console.log("YOU GOT IT TO WORK JENN!!"); //always test to make sure files are set up correctly
+
+var http = require('http'); // http is a node module - creates a web server for us.
+
+http.createServer(function(request, response){ // use this method to create a server "function" is something called a callback
+response.end('Hello, World!');
+ console.log("Your server is running on port 3000");
+}).listen(3000);
