b01a278c32f6ae1254cf45080f55af10efa32f81
jenntress <jenntress@gmail.com>
2017-03-22 21:12:19 -0600

    adding the express files

---
+++ README.md
@@ -0,0 +1,9 @@
+A node project must have a package.json...
+You can create one manually, but the easiest way to
+add one to your project is to run the command 'npm init'
+
+
+- The app.get is getting my files in my directory in my server.js
+ - GET our HTTP method
+- '/greeting' the location we need to go to make a request
+- req, res client makes request, server sends response
--- server.js
+++ server.js
@@ -1,8 +1,31 @@
-console.log("YOU GOT IT TO WORK JENN!!"); //always test to make sure files are set up correctly
+//console.log("YOU GOT IT TO WORK JENN!!"); //always test to make sure files are set up correctly
 
-var http = require('http'); // http is a node module - creates a web server for us.
 
-http.createServer(function(request, response){ // use this method to create a server "function" is something called a callback
-response.end('Hello, World!');
- console.log("Your server is running on port 3000");
-}).listen(3000);
+var express = require('express');
+var app = express();
+
+app.get('/greeting', function(req, res){  //the /greeting is the "URL end point"
+  res.send("You foudn the greeting endpoint!")
+});
+
+console.log(express);
+
+
+
+
+
+// configure our server - spin up a little web server on our local computer
+var server = app.listen(3000, function(){
+  console.log("App up on port 3000") // console log to tell me what port it's running on
+});
+
+
+//made two new endpoints... one that sends a happy and one that sends a sad
+
+app.get('/happy', function(req, res){
+  res.send("Bleep bloop boop bleep boop")
+});
+
+app.get('/sad', function(req, res){
+  res.send("Wha whaaaa")
+});
